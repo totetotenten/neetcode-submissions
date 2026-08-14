7b36e1b480c0edb52049fc1e1842c929950dabbf
Tenga <153099697+totetotenten@users.noreply.github.com>
2026-08-14 13:59:15 +0900

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        d = {}
+        for course in prerequisites:
+            before, after = course
+            d.setdefault(after, []).append(before)
+
+        def search(out_seen, in_seen, course):
+            if course in in_seen:
+                return False
+            if course in out_seen:
+                return True
+            in_seen.add(course)
+            out_seen.add(course)
+
+            if course not in d:
+                in_seen.remove(course)
+                return True
+            for before in d[course]:
+                if not search(out_seen, in_seen, before):
+                    return False
+            in_seen.remove(course)
+            return True
+        
+        out_seen = set()
+        b = True
+        for i in range(0, numCourses):
+            in_seen = set()
+            if not search(out_seen, in_seen, i):
+                b = False
+            
+        return b
+        
