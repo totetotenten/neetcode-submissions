00e6bf1e416a6c08a0b44c0d9c8f83d0ae4fb6a0
Tenga <153099697+totetotenten@users.noreply.github.com>
2026-08-31 17:49:26 +0900

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        seen = {}
+        seen[0] = 0
+        for i in range(1, n+1):
+            if i % 2 == 1:
+                seen[i] = seen[i-1] + 1
+            else:
+                seen[i] = seen[i/2]
+        return(list(seen.values()))
