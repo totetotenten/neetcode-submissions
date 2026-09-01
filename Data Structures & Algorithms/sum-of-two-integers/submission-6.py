bb03d94ad20622f25d65fa927186c3bbbf3f2cfb
Tenga <153099697+totetotenten@users.noreply.github.com>
2026-09-01 11:38:00 +0900

    Add: sum-of-two-integers - submission-6

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        result = 0
+        carry = 0
+        mask = 0xFFFFFFFF
+        for i in range(32):
+            dig_a = (a & (1 << i)) >> i
+            dig_b = (b & (1 << i)) >> i
+            dig_tmp = dig_a ^ dig_b ^ carry
+            carry = (dig_a & dig_b) | (dig_a & carry) | (carry & dig_b)
+            result = result | (dig_tmp << i)
+
+        if (result & (1<<31))>>31 == 1:
+            result = ~(result^mask)
+        return result
+
